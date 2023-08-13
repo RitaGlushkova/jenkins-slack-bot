7c337d142a07228548436c60344e9d24beea8471
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2023-08-13 20:20:24 +0100

    add jenkinsfile

---
+++ basic-trials/slack-basic-thread.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	errEnv := godotenv.Load("./slack.env")
+	if errEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	message := "Hello from your Go Slack bot!"
+	threadMessage := "Hello you!"
+
+	// Send the main message
+	_, ts, _, err := client.SendMessage(os.Getenv("CHANNEL_ID"), slack.MsgOptionText(message, false))
+	if err != nil {
+		log.Fatalf("Error sending main message: %v", err)
+	}
+	time.Sleep(2 * time.Second)
+	// Send the second message in the thread
+	_, _, _, err = client.SendMessage(os.Getenv("CHANNEL_ID"), slack.MsgOptionText(threadMessage, false), slack.MsgOptionTS(ts))
+	if err != nil {
+		log.Fatalf("Error sending thread message: %v", err)
+	}
+
+	fmt.Println("Messages sent to Slack successfully!")
+}
