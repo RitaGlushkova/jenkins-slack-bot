94cb4b9754dccaa848d531c4685d4b3a502f9e08
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2023-08-13 22:23:54 +0100

    trying silly thread

--- send-jenkins-notifications/send-notification.go
+++ send-jenkins-notifications/send-notification.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/slack-go/slack"
@@ -14,37 +15,80 @@ func main() {
 	// }
 	args := os.Args[1:]
 	fmt.Println(args)
-
+	CHANNEL_ID := "C05LKP4G32P"
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
-	fmt.Println(os.Getenv("SLACK_BOT_TOKEN"))
-	preText := "*Hello! Your Jenkins build has finished!*"
-	jenkinsURL := "*Build URL:* " + args[0]
-	buildResult := "*" + args[1] + "*"
-	buildNumber := "*" + args[2] + "*"
-	jobName := "*" + args[3] + "*"
-
-	if buildResult == "*SUCCESS*" {
-		buildResult = buildResult + " :white_check_mark:"
-	} else {
-		buildResult = buildResult + " :x:"
+	startMsg := "Hello! This is your jenkins build update :thread!"
+	if args[0] == "CHECKOUT" {
+		_, _, _, err := api.SendMessage(CHANNEL_ID, slack.MsgOptionText(startMsg, false))
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		return
 	}
 
-	dividerSection1 := slack.NewDividerBlock()
-	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
-	preTextFiend := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
-	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
+	// Building a thread
+	searchText := "Hello! This is your jenkins build update :thread!" // Replace with the text you want to search for
 
-	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
-	preTextSection := slack.NewSectionBlock(preTextFiend, nil, nil)
+	historyParams := slack.GetConversationHistoryParameters{
+		ChannelID: CHANNEL_ID,
+	}
 
-	msg := slack.MsgOptionBlocks(
-		preTextSection,
-		dividerSection1,
-		jenkinsBuildDetailsSection,
-	)
-	_, _, _, err := api.SendMessage("C05LKP4G32P", msg)
+	messages, err := api.GetConversationHistory(&historyParams)
 	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
+		log.Fatalf("Error fetching conversation history: %s", err)
+	}
+	var ts string
+	for _, message := range messages.Messages {
+		if message.Text == searchText {
+			ts = message.Timestamp
+			break
+		}
+	}
+
+	if len(messages.Messages) == 0 {
+		fmt.Println("No messages found with the specified text.")
+	}
+
+	if args[0] == "BUILD" {
+		buildMsg := "Your build has started!"
+		_, ts, _, err = api.SendMessage(CHANNEL_ID, slack.MsgOptionText(buildMsg, false), slack.MsgOptionTS(ts))
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+	}
+	if args[0] == "DONE" {
+		//building final message
+		preText := "*Hello! Your Jenkins build has finished!*"
+		jenkinsURL := "*Build URL:* " + args[1]
+		buildResult := "*" + args[2] + "*"
+		buildNumber := "*" + args[3] + "*"
+		jobName := "*" + args[4] + "*"
+
+		if buildResult == "*SUCCESS*" {
+			buildResult = buildResult + " :white_check_mark:"
+		} else {
+			buildResult = buildResult + " :x:"
+		}
+
+		dividerSection1 := slack.NewDividerBlock()
+		jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
+		preTextFiend := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
+		jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
+
+		jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+		preTextSection := slack.NewSectionBlock(preTextFiend, nil, nil)
+
+		msg := slack.MsgOptionBlocks(
+			preTextSection,
+			dividerSection1,
+			jenkinsBuildDetailsSection,
+		)
+		_, _, _, err = api.SendMessage(CHANNEL_ID, msg, slack.MsgOptionTS(ts))
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
 	}
 }
