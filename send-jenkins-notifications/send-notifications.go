7c758ef1b41c49ba6e57460b175362b6e60a7d28
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2023-08-28 18:18:18 +0100

    simple mircoservice

--- send-jenkins-notifications/send-notifications.go
+++ send-jenkins-notifications/send-notifications.go
@@ -4,38 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	"net/http"
+
 	"github.com/slack-go/slack"
 )
 
-func main() {
-	args := os.Args[1:]
-	fmt.Println(args)
-
+func sendSlackMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Send Slack Message</h1>")
 	client := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	headerText := "*Hello! This is your jenkins build has finished!*"
-	jenkinsURL := "*Jenkins URL:* " + args[0]
-	buildResult := "*" + args[1] + "*"
-	buildNumber := "*" + args[2] + "*"
-	jobName := "*" + args[3] + "*"
-
-	if buildResult == "*SUCCESS*" {
-		buildResult = buildResult + "* :tada:"
-	} else {
-		buildResult = buildResult + "* :x:"
-	}
-
 	dividerSection1 := slack.NewDividerBlock()
-	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
 	headerField := slack.NewTextBlockObject("mrkdwn", headerText+"\n\n", false, false)
-	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
-
-	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
 	headerSection := slack.NewSectionBlock(headerField, nil, nil)
 
 	msg := slack.MsgOptionBlocks(
 		headerSection,
 		dividerSection1,
-		jenkinsBuildDetailsSection,
 	)
 
 	_, _, err := client.PostMessage("C05LKP4G32P", msg)
@@ -43,4 +27,9 @@ func main() {
 		fmt.Printf("%s\n", err)
 		return
 	}
+}
+
+func main() {
+	http.HandleFunc("/sendSlackMessage", sendSlackMessage)
+	http.ListenAndServe(":8091", nil)
 }
